17dfc10f63221c1a09825cad2beb86373f0f0c27
GSM-Minseok <ms0818000@gmail.com>
2023-04-05 21:02:02 +0900

    QRcode webview

--- app/src/main/java/com/example/qrcodescan/MainActivity.kt
+++ app/src/main/java/com/example/qrcodescan/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.example.qrcodescan
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.webkit.WebViewClient
+import android.widget.Toast
 import com.example.qrcodescan.databinding.ActivityMainBinding
 import com.google.zxing.integration.android.IntentIntegrator
 
@@ -19,7 +23,30 @@ class MainActivity : AppCompatActivity() {
             integrator.setCameraId(0)
             integrator.setBeepEnabled(true)
             integrator.setBarcodeImageEnabled(false)
+            integrator.setOrientationLocked(false)
             integrator.initiateScan()
+        }
+    }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+
+        val result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data)
+        Log.d("testt", "QR 코드 체크")
+
+        if (result != null) {
+            if (result.contents == null) {
+                Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show()
+            }
+            else {
+                Toast.makeText(this, "scanned" + result.contents, Toast.LENGTH_LONG).show()
+                Log.d("TTT", "QR 코드 URL:${result.contents}")
+
+                binding.webView.settings.javaScriptEnabled = true
+                binding.webView.webViewClient = WebViewClient()
+
+                binding.webView.loadUrl(result.contents)
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data)
         }
     }
 }
