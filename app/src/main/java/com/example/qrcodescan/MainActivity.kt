3d1207064f3c80e1aabbc780e6daa3f7f225618c
GSM-Minseok <ms0818000@gmail.com>
2023-04-05 20:42:57 +0900

    QRCode scanner

--- app/src/main/java/com/example/qrcodescan/MainActivity.kt
+++ app/src/main/java/com/example/qrcodescan/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.qrcodescan
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.qrcodescan.databinding.ActivityMainBinding
+import com.google.zxing.integration.android.IntentIntegrator
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnQrScan.setOnClickListener {
+            val integrator = IntentIntegrator(this)
+            integrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE)
+            integrator.setPrompt("QR 코드를 스캔하여 주세요")
+            integrator.setCameraId(0)
+            integrator.setBeepEnabled(true)
+            integrator.setBarcodeImageEnabled(false)
+            integrator.initiateScan()
+        }
     }
 }
